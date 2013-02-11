a0441a341cea369173a5f2267c8203e164c455ec
Bhaskar Sundarraj <bskar.sundarraj@gmail.com>
2013-02-11 14:21:38 -0800

    bumping the version to 0.0.5.

--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe PlainOldModel::Base do
 
-
-  describe "gem version" do
-    it "should return the current version of the gem" do
-      PlainOldModel::VERSION.should == '0.0.4'
-    end
-  end
-
   describe " assign_attribute and new" do
     it "should accept the params and new up a class with variables initialized" do
       @person= Person.new({:fname => "first value", :lname => "second value", :address => {:fname => 'fname', :lname => 'lname'}})
